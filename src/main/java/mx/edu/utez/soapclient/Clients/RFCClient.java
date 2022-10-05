d4d6b9d41334636c6b1fea551cadfcce24eb5bf8
DavidRos1231 <20213tn038@utez.edu.mx>
2022-10-05 09:44:40 -0500

    second

--- src/main/java/mx/edu/utez/soapclient/Clients/RFCClient.java
+++ src/main/java/mx/edu/utez/soapclient/Clients/RFCClient.java
@@ -5,23 +5,42 @@ import java.util.Scanner;
 
 public class RFCClient {
     public static void main(String[] args) {
+        System.out.println("QUe desea hacer \n1.-RFC\n2.-NumeroAleatorio\n3.-SinVocales");
         Scanner sc=new Scanner(System.in);
         RFCServiceService service= new RFCServiceService();
         Service port=service.getServicePort();
-        System.out.println("Ingresa tu nombre");
-        String name=sc.nextLine();
-        System.out.println("Ingresa tu apellido paterno");
-        String last=sc.nextLine();
-        System.out.println("Ingresa tu apelido materno");
-        String mid=sc.nextLine();
-        System.out.println("Ingresa tu año de naciemiento");
-        String ano=sc.nextLine();
-        System.out.println("Ingresa tu Mes de nacimiento (En numero)");
-        String mes=sc.nextLine();
-        System.out.println("Ingresa tu dia de nacimiento");
-        String dia=sc.nextLine();
+        String response;
+        switch (Integer.parseInt(sc.nextLine())){
+            case 1:
+                System.out.println("Ingresa tu nombre");
+                String name=sc.nextLine();
+                System.out.println("Ingresa tu apellido paterno");
+                String last=sc.nextLine();
+                System.out.println("Ingresa tu apelido materno");
+                String mid=sc.nextLine();
+                System.out.println("Ingresa tu dia de nacimiento");
+                String dia=sc.nextLine();
+                System.out.println("Ingresa tu Mes de nacimiento (En numero)");
+                String mes=sc.nextLine();
+                System.out.println("Ingresa tu año de naciemiento");
+                String ano=sc.nextLine();
+                response=port.rfcgen(name,mid,last,ano,mes,dia);
+                System.out.println(response);
+                break;
+            case 2:
+                do {
+                    System.out.println("Coloca un numero");
+                    response=port.reponseMessage(sc.nextInt());
+                    System.out.println(response);
+                }while (response.charAt(0)=='I');
+                break;
+            case 3:
+                System.out.println("Escribe una frase");
+                response=port.sinvocales(sc.nextLine());
+                System.out.println(response);
+                break;
+        }
 
-        String response=port.rfcgen(name,mid,last,ano,mes,dia);
-        System.out.println(response);
+
     }
 }
